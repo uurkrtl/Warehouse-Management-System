1556fd63b1f5b51729a3adff387b55c5dfafc4be
Ugur Kartal <kartalugur@gmail.com>
2024-02-18 13:35:18 +0100

    add id annotation on Category.java

--- WMS-Service/src/main/java/net/ugurkartal/wmsservice/models/Category.java
+++ WMS-Service/src/main/java/net/ugurkartal/wmsservice/models/Category.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.With;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
@@ -14,6 +15,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @With
 public class Category {
+    @Id
     private String id;
     private String name;
     private String description;
