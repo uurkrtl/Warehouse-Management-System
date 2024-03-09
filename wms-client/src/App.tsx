98b01a96edfbd558672f88ac5b2d6a6f48fd5b66
Ugur Kartal <kartalugur@gmail.com>
2024-03-09 14:55:54 +0100

    setup frontend project

---
+++ wms-client/src/App.tsx
@@ -0,0 +1,11 @@
+
+function App() {
+  return (
+    <>
+        <h1>WMS Client</h1>
+        <p>WMS Client is a web-based application for managing a warehouse.</p>
+    </>
+  )
+}
+
+export default App
