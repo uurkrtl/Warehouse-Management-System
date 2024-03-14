4c443972af27706e3a8a1f0e8723e2d598a267bd
Ugur Kartal <kartalugur@gmail.com>
2024-03-14 21:36:00 +0100

    add StockNotZeroException

--- WMS-Service/src/main/java/net/ugurkartal/wmsservice/services/validations/ProductValidation.java
+++ WMS-Service/src/main/java/net/ugurkartal/wmsservice/services/validations/ProductValidation.java
@@ -3,6 +3,7 @@ package net.ugurkartal.wmsservice.services.validations;
 import lombok.RequiredArgsConstructor;
 import net.ugurkartal.wmsservice.core.exception.DuplicateRecordException;
 import net.ugurkartal.wmsservice.core.exception.RecordNotFoundException;
+import net.ugurkartal.wmsservice.core.exception.StockNotZeroException;
 import net.ugurkartal.wmsservice.models.Product;
 import net.ugurkartal.wmsservice.repositories.ProductRepository;
 import org.springframework.stereotype.Service;
@@ -32,6 +33,16 @@ public class ProductValidation {
             Product product = productOptional.get();
             if(!product.getName().equals(productName) && this.productRepository.existsByName(productName)) {
                 throw new DuplicateRecordException("Product name already exists: " + productName);
+            }
+        }
+    }
+
+    public void checkIfProductStockNotZero(String id) {
+        Optional<Product> productOptional = this.productRepository.findById(id);
+        if(productOptional.isPresent()) {
+            Product product = productOptional.get();
+            if(product.getStock() > 0) {
+                throw new StockNotZeroException("Product stock is not zero. Product name: " + product.getName());
             }
         }
     }
