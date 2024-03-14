64b9d1c1a6ef945f3852d0f8079d598f96b3d724
Ugur Kartal <kartalugur@gmail.com>
2024-03-14 15:05:34 +0100

    add ProductUpdate

--- wms-client/src/layouts/Dashboard.tsx
+++ wms-client/src/layouts/Dashboard.tsx
@@ -1,6 +1,8 @@
 import {Route, Routes} from "react-router-dom";
 import ProductList from "../pages/products/ProductList.tsx";
 import ProductAdd from "../pages/products/ProductAdd.tsx";
+import ProductUpdate from "../pages/products/ProductUpdate.tsx";
+import PageNotFound404 from "../pages/PageNotFound404.tsx";
 
 
 function Dashboard() {
@@ -9,6 +11,9 @@ function Dashboard() {
             <Routes>
                 <Route path={'/products'} element={<ProductList/>}/>
                 <Route path={'/products/add'} element={<ProductAdd/>}/>
+                <Route path={'/products/update/:id'} element={<ProductUpdate/>}/>
+                <Route path={'/'} element={<ProductList/>}/>
+                <Route path={'*'} element={<PageNotFound404/>}/>
             </Routes>
         </div>
     );
--- wms-client/src/pages/products/ProductUpdate.tsx
+++ wms-client/src/pages/products/ProductUpdate.tsx
@@ -1,15 +1,18 @@
-import {Product} from "../../types/Product.ts";
 import {useEffect, useState} from "react";
-import {useNavigate} from "react-router-dom";
+import {Product} from "../../types/Product.ts";
+import {useNavigate, useParams} from "react-router-dom";
 import ProductService from "../../services/ProductService.ts";
-import {Category} from "../../types/Category.ts";
 import CategoryService from "../../services/CategoryService.ts";
+import {Category} from "../../types/Category.ts";
 
+const productService = new ProductService();
+const categoryService = new CategoryService();
 
-function ProductAdd() {
+function ProductUpdate() {
+    const { id = '' } = useParams<string>();
     const [product, setProduct] = useState<Product>({
         name: '',
-        id: '',
+        id: id,
         description: '',
         salePrice: 0,
         stock: 0,
@@ -19,11 +22,9 @@ function ProductAdd() {
         categoryName: '',
         createdAt: new Date(),
         updatedAt: new Date(),
-        active: false
+        active: true
     });
     const navigate = useNavigate();
-    const productService = new ProductService();
-    const categoryService = new CategoryService();
     const [categories, setCategories] = useState<Category[]>([]);
     const [errorMessage, setErrorMessage] = useState<string>('');
 
@@ -33,9 +34,22 @@ function ProductAdd() {
         });
     });
 
+    useEffect(() => {
+        if (id) {
+            productService.getByIdProduct(id)
+                .then((response) => {
+                    setProduct(response.data);
+                })
+                .catch((error) => {
+                    console.error('Error fetching product:', error);
+                    navigate('*');
+                });
+        }
+    }, [id, navigate]);
+
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        productService.addProduct(product)
+        productService.updateProduct(id, product)
             .then(response => {
                 console.log(response)
                 navigate('/products')
@@ -61,7 +75,7 @@ function ProductAdd() {
                             <path
                                 d="M8 1a2 2 0 0 1 2 2v2H6V3a2 2 0 0 1 2-2m3 4V3a3 3 0 1 0-6 0v2H3.36a1.5 1.5 0 0 0-1.483 1.277L.85 13.13A2.5 2.5 0 0 0 3.322 16h9.355a2.5 2.5 0 0 0 2.473-2.87l-1.028-6.853A1.5 1.5 0 0 0 12.64 5zm-1 1v1.5a.5.5 0 0 0 1 0V6h1.639a.5.5 0 0 1 .494.426l1.028 6.851A1.5 1.5 0 0 1 12.678 15H3.322a1.5 1.5 0 0 1-1.483-1.723l1.028-6.851A.5.5 0 0 1 3.36 6H5v1.5a.5.5 0 1 0 1 0V6z"/>
                         </svg>
-                        <h2>Produkt hinzufügen</h2>
+                        <h2>Produktaktualisierung</h2>
                     </div>
 
                     <div className="row g-5">
@@ -72,38 +86,47 @@ function ProductAdd() {
                                     <div className="col-sm-6">
                                         <label htmlFor="name" className="form-label">Produktname</label>
                                         <input type="text" className="form-control" id="name"
-                                               placeholder="Schreiben Sie den Produktnamen"
+                                               placeholder="Schreiben Sie den Produktnamen" value={product.name}
                                                onChange={(e) => setProduct({...product, name: e.target.value})}/>
                                     </div>
 
                                     <div className="col-sm-6">
                                         <label htmlFor="description" className="form-label">Beschreibung</label>
                                         <input type="text" className="form-control" id="description"
-                                               placeholder="Schreiben Sie die Beschreibung"
+                                               placeholder="Schreiben Sie die Beschreibung" value={product.description}
                                                onChange={(e) => setProduct({...product, description: e.target.value})}/>
                                     </div>
 
                                     <div className="col-sm-4">
                                         <label htmlFor="salePrice" className="form-label">Verkaufspreis</label>
                                         <input type="number" step="0.01" className="form-control" id="salePrice"
-                                               placeholder="Schreiben Sie den Verkaufspreis"
-                                               onChange={(e) => setProduct({...product, salePrice: e.target.valueAsNumber})}/>
+                                               placeholder="Schreiben Sie den Verkaufspreis" value={product.salePrice}
+                                               onChange={(e) => setProduct({
+                                                   ...product,
+                                                   salePrice: e.target.valueAsNumber
+                                               })}/>
                                     </div>
 
                                     <div className="col-sm-4">
                                         <label htmlFor="criticalStock" className="form-label">Kritischer
                                             Lagerbestand</label>
                                         <input type="number" className="form-control" id="criticalStock"
-                                               placeholder="Schreiben Sie den kritischen Lagerbestand"
-                                               onChange={(e) => setProduct({...product, criticalStock: e.target.valueAsNumber})}/>
+                                               placeholder="Schreiben Sie den kritischen Lagerbestand" value={product.criticalStock}
+                                               onChange={(e) => setProduct({
+                                                   ...product,
+                                                   criticalStock: e.target.valueAsNumber
+                                               })}/>
                                     </div>
 
                                     <div className="col-sm-4">
                                         <label htmlFor="categoryName" className="form-label">Kategoriename</label>
-                                        <select className="form-select" id="categoryName" onChange={(e) => setProduct({...product, categoryId: e.target.value})}>
-                                            {categories.map((category) => {
+                                        <select className="form-select" id="categoryName"
+                                                onChange={(e) => setProduct({...product, categoryId: e.target.value})}>
+                                            <option value={product.categoryId}>{product.categoryName}</option>
+                                            {categories.filter((category=>category.name!==product.categoryName)).map((category)=> {
                                                 return (
-                                                    <option key={category.id} value={category.id}>{category.name}</option>
+                                                    <option key={category.id}
+                                                            value={category.id}>{category.name}</option>
                                                 )
                                             })};
                                         </select>
@@ -112,12 +135,13 @@ function ProductAdd() {
                                     <div className="col-sm-12">
                                         <label htmlFor="imageUrl" className="form-label">Bild URL</label>
                                         <input type="text" className="form-control" id="imageUrl"
-                                               placeholder="Schreiben Sie die Bild URL"
+                                               placeholder="Schreiben Sie die Bild URL" value={product.imageUrl}
                                                onChange={(e) => setProduct({...product, imageUrl: e.target.value})}/>
                                     </div>
                                 </div>
 
-                                <button className="w-100 btn btn-primary btn-lg my-4" type="submit">Speichern</button>
+                                <button className="w-100 btn btn-primary btn-lg my-4" type="submit">Aktualisieren
+                                </button>
 
                             </form>
 
@@ -135,4 +159,4 @@ function ProductAdd() {
     );
 }
 
-export default ProductAdd;
+export default ProductUpdate;
